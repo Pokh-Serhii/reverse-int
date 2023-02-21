fd42c54f130e56e8c0842eb05368b95e43646ca1
Serhii Pokh <117908961+Pokh-Serhii@users.noreply.github.com>
2023-02-21 11:18:04 +0200

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const numberPrepared = Math.abs(n);
+    const numberString = numberPrepared.toString();
+    const numberArray = numberString.split('');
+    const result = numberArray.reverse().join('');
+
+    return Number(result);  
 }
